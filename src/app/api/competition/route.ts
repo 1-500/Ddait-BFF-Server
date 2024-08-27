71fa7e2fce0b8d5d40edce3e7b8c243a448d8196
aong13 <oaoa13@pukyong.ac.kr>
2024-08-27 16:33:09 +0900

    ISSUE-147 :bug: Fix: competition_record에 post하는 부분 삭제
    - 창현님 코드 사용해서 유저가 경쟁방에 입장하도록 클라이언트측에서 요청 예정
    - 결과값으로 roomId를 반환 -> room 입장에 사용

--- src/app/api/competition/route.ts
+++ src/app/api/competition/route.ts
@@ -14,7 +14,6 @@ export async function POST(req: NextRequest) {
       end_date,
       is_private,
       smartwatch,
-      user_id,
     } = await req.json()
 
     const { data: roomData, error: roomError } = await supabase.from('competition_room').insert([
@@ -33,28 +32,10 @@ export async function POST(req: NextRequest) {
     if (roomError) {
       return NextResponse.json({ message: `경쟁방 생성 중 오류 발생: ${roomError.message}` }, { status: 400 })
     }
-    
-    // member_id의 유효성 확인 (로그인 후 유저데이터 저장이 잘못된 경우 대비)
-    const { data: memberData, error: memberError } = await supabase.from('member').select('id').eq('id', user_id).single()
-    if (memberError || !memberData) {
-      return NextResponse.json({ message: '유효하지 않은 사용자 ID입니다.' }, { status: 400 })
-    }
-
-    const { data: entryData, error: entryError } = await supabase.from('competition_record').insert([
-      {
-        member_id: user_id,
-        competition_room_id: roomData.id,
-      },
-    ]).select().single()
-
-    if (entryError) {
-      return NextResponse.json({ message: `경쟁방에 유저 ${user_id}가 입장하는데 오류가 발생했습니다: ${entryError.message}` }, { status: 400 })
-    }
 
     const responseData = {
       room_id: roomData.id, 
       title: roomData.title,
-      user: entryData.member_id
     }
 
     return NextResponse.json({ message: '경쟁방이 성공적으로 생성되었습니다.', data: responseData }, { status: 201 })
