130cd50e8d5415e745147cf11a9960bbd74dc7c8
aong13 <oaoa13@pukyong.ac.kr>
2024-09-03 14:35:27 +0900

    ISSUE-197 :bug: Fix: 일치하는 결과 없는 경우 status 변경
    - 응답이 성공이지만 data가 비어있는 경우 204로 처리

--- src/app/api/users/search/route.ts
+++ src/app/api/users/search/route.ts
@@ -21,7 +21,7 @@ export async function GET(request: NextRequest) {
     }
 
     if (data.length === 0) {
-      return NextResponse.json({ message: `${nickname}을 포함한 닉네임을 가진 유저가 없어요.`, data}, { status: 200 })
+      return NextResponse.json({ message: `${nickname}을 포함한 닉네임을 가진 유저가 없어요.`}, { status:  204 })
     }
 
     return NextResponse.json(data, { status: 200 })
