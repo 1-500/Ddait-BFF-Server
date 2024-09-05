7508cf9ec02bc8d9cf9bd7ecc741362f684d1f77
aong13 <oaoa13@pukyong.ac.kr>
2024-09-05 23:45:09 +0900

    ISSUE-263 ♻️ Refactor: 공통된 처리 util로 분리 / join 사용
    - util 함수의 props를 받아서 다르게 처리
    - join 후 id를 비교해서 내 정보 빼고 조회하는 방식

--- src/app/api/friends/request/received/route.ts
+++ src/app/api/friends/request/received/route.ts
@@ -1,10 +1,9 @@
-import { NextResponse, NextRequest } from 'next/server';
-import { createClient } from '@/utils/supabase/client';
+import { NextResponse, NextRequest } from 'next/server'
+import { getFriendsWithDetails } from '@/utils/friendUtils'
 
 export async function GET(req: NextRequest) {
   try {
-    const supabase = createClient();
-    const userId = req.headers.get('X-User-Id');
+    const userId = req.headers.get('X-User-Id')
 
     if (!userId) {
       return NextResponse.json(
@@ -14,64 +13,32 @@ export async function GET(req: NextRequest) {
           message: '사용자 ID가 필요합니다.',
         },
         { status: 400 }
-      );
+      )
     }
 
-    const { data: friendsData, error: friendsError } = await supabase
-      .from('friends')
-      .select('member_id, friend_member_id, status')
-      .or(`friend_member_id.eq.${userId}`)
-      .eq('status', '대기 중');
-
-    if (friendsError) {
-      return NextResponse.json(
-        {
-          status: friendsError.status || 500,
-          code: 'FRIENDS_LIST_ERROR',
-          message: friendsError.message || '보낸 친구 신청 목록 조회 중 오류가 발생했습니다.',
-        },
-        { status: friendsError.status || 500 }
-      );
-    }
-
-    // 친구 목록을 member 테이블에서 가져온 친구의 상세 정보로 변환
-    const friendIds = friendsData.map(friend => {
-      return friend.member_id === userId ? friend.friend_member_id : friend.member_id;
-    });
-
-    const { data: friendsDetails, error: detailsError } = await supabase
-      .from('member')
-      .select('id, introduce, profile_image, nickname, preferred_sport')
-      .in('id', friendIds);
-
-    if (detailsError) {
-      return NextResponse.json(
-        {
-          status: detailsError.status || 500,
-          code: 'FRIENDS_DETAILS_ERROR',
-          message: detailsError.message || '친구 상세 정보 조회 중 오류가 발생했습니다.',
-        },
-        { status: detailsError.status || 500 }
-      );
-    }
+    const friendsData = await getFriendsWithDetails({
+      userId,
+      status: '대기 중',
+      type: 'received',
+    })
 
     return NextResponse.json(
       {
         status: 200,
         code: 'SUCCESS',
-        message: '보낸 친구 신청 목록 조회에 성공했습니다.',
-        data: friendsDetails,
+        message: '받은 친구 요청 목록 조회에 성공했습니다.',
+        data: friendsData,
       },
       { status: 200 }
-    );
+    )
   } catch (error) {
     return NextResponse.json(
       {
-        status: 500,
-        code: 'INTERNAL_SERVER_ERROR',
+        status: error.status || 500,
+        code: error.code || 'INTERNAL_SERVER_ERROR',
         message: error.message || '예상치 못한 오류가 발생했습니다.',
       },
-      { status: 500 }
-    );
+      { status: error.status || 500 }
+    )
   }
 }
---
+++ src/utils/friendUtils.ts
@@ -0,0 +1,46 @@
+import { createClient } from '@/utils/supabase/client'
+
+export async function getFriendsWithDetails({ userId, status, type }) {
+  const supabase = createClient()
+  
+  // 조인 사용
+  const query = supabase
+    .from('friends')
+    .select(
+      `status, 
+      member_id, 
+      friend_member_id, 
+      member:member_id(id, introduce, profile_image, nickname, preferred_sport), 
+      friend:friend_member_id(id, introduce, profile_image, nickname, preferred_sport)`
+    )
+    .eq('status', status)
+
+  // 친구 요청 타입에 따라 쿼리 조건 변경
+  switch (type) {
+    case 'received':
+      query.or(`friend_member_id.eq.${userId}`)
+      break
+    case 'sent':
+      query.or(`member_id.eq.${userId}`)
+      break
+    case 'friends':
+      query.or(`member_id.eq.${userId},friend_member_id.eq.${userId}`)
+      break
+  }
+
+  const { data: friendsData, error: friendsError } = await query
+
+  if (friendsError) {
+    throw {
+      status: friendsError.status || 500,
+      code: 'MEMBER_FRIEND_JOIN_ERROR',
+      message: friendsError.message || 'member테이블과 friends테이블 조인 중 오류가 발생했습니다.',
+    }
+  }
+
+  // 상대방의 정보만 필터링( memberId가 내 아이디면 friendId기준 조회해야 상대방 데이터임. 반대 경우도 동일)
+  const friendDetails = friendsData.map(friend => 
+    friend.member_id === userId ? friend.friend : friend.member
+  )
+  return friendDetails
+}
