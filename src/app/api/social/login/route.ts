c88a6756fbfa9962c8465fd0b3bb337567d1483e
nara04040 <nara04040@naver.com>
2024-10-02 14:04:02 +0900

    ISSUE-319 :spakles: Feat: 애플 로그인 JWT검증 로직 구현

--- src/app/api/social/login/route.ts
+++ src/app/api/social/login/route.ts
@@ -1,27 +1,54 @@
-import { createClient } from '@/utils/supabase/server'
-import { cookies } from 'next/headers'
+import jwt from 'jsonwebtoken'
+import jwkToPem from 'jwk-to-pem' // JWK를 PEM으로 변환하기 위한 라이브러리
+import { NextResponse } from 'next/server'
 
-import { NextRequest, NextResponse } from 'next/server'
-
-export async function GET(req: NextRequest) {
+export async function POST(request: Request) {
   try {
-    const cookieStore = cookies()
-    const supabase = createClient(cookieStore)
+    const { identityToken } = await request.json()
+    console.log('Received identityToken:', identityToken)
 
-    const { searchParams } = new URL(req.url)
-    const email = searchParams.get('email')
+    // Apple의 공개 키 가져오기
+    const response = await fetch('https://appleid.apple.com/auth/keys')
+    const appleKeys = await response.json()
+    console.log('Apple public keys:', appleKeys)
 
-    const searchResult = await supabase.from('social_login').select('*').eq('email', email).single()
-    const memberResult = await supabase.from('member').select('*').eq('id', searchResult.data.member_id).single()
+    // JWT 헤더에서 kid 추출
+    const decodedHeader = jwt.decode(identityToken, { complete: true })
+    const { kid } = decodedHeader?.header
 
-    const signInResult = await supabase.auth.signInWithPassword({
-      email: memberResult.data.email,
-      password: memberResult.data.password,
+    // kid와 일치하는 공개 키 찾기
+    const applePublicKey = appleKeys.keys.find((key: any) => key.kid === kid)
+
+    if (!applePublicKey) {
+      throw new Error('Public key not found')
+    }
+
+    // JWK를 PEM으로 변환
+    const pem = jwkToPem(applePublicKey)
+
+    // JWT 검증
+    const decodedToken = jwt.verify(identityToken, pem, {
+      algorithms: ['RS256'],
+      issuer: 'https://appleid.apple.com',
     })
 
-    return NextResponse.json(signInResult.data.session)
+    console.log('Decoded Token:', decodedToken)
+
+    const { sub: appleUserId, email } = decodedToken as any
+
+    return NextResponse.json({
+      message: 'Apple login successful',
+      appleUserId,
+      email,
+    })
   } catch (error) {
-    console.log(error)
-    return NextResponse.json({ message: error })
+    console.error('Apple login error:', error)
+    return NextResponse.json(
+      {
+        message: 'Apple login failed',
+        error: (error as Error).message,
+      },
+      { status: 400 },
+    )
   }
 }
