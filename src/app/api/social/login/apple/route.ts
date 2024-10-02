be090c1c7a30c8b894e9604a7a106a8c69ffd2ed
nara04040 <nara04040@naver.com>
2024-10-02 14:56:54 +0900

    Docs: 주석 처리 삭제

--- src/app/api/social/login/apple/route.ts
+++ src/app/api/social/login/apple/route.ts
@@ -6,13 +6,11 @@ import { NextResponse } from 'next/server'
 
 export async function POST(request: Request) {
   try {
-    // Supabase 클라이언트 초기화
     const cookieStore = cookies()
     const supabase = createClient(cookieStore)
     const { identityToken } = await request.json()
     console.log('Received identityToken:', identityToken)
 
-    // Apple의 공개 키 가져오기
     const response = await fetch('https://appleid.apple.com/auth/keys')
     const appleKeys = await response.json()
     console.log('Apple public keys:', appleKeys)
@@ -28,10 +26,8 @@ export async function POST(request: Request) {
       throw new Error('Public key not found')
     }
 
-    // JWK를 PEM으로 변환
     const pem = jwkToPem(applePublicKey)
 
-    // JWT 검증
     const decodedToken = jwt.verify(identityToken, pem, {
       algorithms: ['RS256'],
       issuer: 'https://appleid.apple.com',
@@ -41,19 +37,17 @@ export async function POST(request: Request) {
 
     const { sub: appleUserId, email } = decodedToken as any
 
-    // Supabase에서 해당 사용자가 있는지 확인
     let { data: member, error: memberError } = await supabase.from('member').select('*').eq('email', email).single()
 
-    // 사용자가 없으면 새로운 사용자 생성
     if (!member) {
       const { data: newMember, error: newMemberError } = await supabase
         .from('member')
         .insert([
           {
             email,
-            nickname: 'Apple User', // 기본 닉네임 설정
-            user_level: 0, // 기본 사용자 레벨
-            birthdate: '1970-01-01', // 기본 생년월일 설정
+            nickname: 'Apple User',
+            user_level: 0,
+            birthdate: '1970-01-01',
           },
         ])
         .select()
@@ -66,7 +60,6 @@ export async function POST(request: Request) {
       member = newMember
     }
 
-    // social_login 테이블에 소셜 로그인 정보 저장 또는 업데이트
     const { data: socialLogin, error: socialLoginError } = await supabase
       .from('social_login')
       .upsert({
@@ -74,7 +67,7 @@ export async function POST(request: Request) {
         provider_user_id: appleUserId,
         email,
         user_level: member.user_level,
-        member_id: member.id, // member 테이블의 UUID 연결
+        member_id: member.id,
       })
       .select()
       .single()
@@ -83,9 +76,8 @@ export async function POST(request: Request) {
       throw new Error('소셜 로그인 저장 오류: ' + socialLoginError.message)
     }
 
-    // 인증 성공 시 클라이언트로 응답
     return NextResponse.json({
-      success: true, // 클라이언트에서 사용되는 success 플래그
+      success: true,
       message: 'Apple login successful',
       member,
       socialLogin,
